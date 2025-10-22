ad8571b04510a5975773c09f97aae98fb358e0c6
grantdfoster <grantdfoster@gmail.com>
2025-10-22 03:05:01 +0200

    fix: generic args for tiktok

--- api/params/tiktok.go
+++ api/params/tiktok.go
@@ -1,8 +1,8 @@
 package params
 
 import (
-	"github.com/masa-finance/tee-worker/api/args/base"
 	"github.com/masa-finance/tee-worker/api/args/tiktok"
+	"github.com/masa-finance/tee-worker/api/types"
 )
 
 type TikTokTranscription = Params[*tiktok.TranscriptionArguments]
@@ -11,4 +11,19 @@ type TikTokSearch = Params[*tiktok.QueryArguments]
 
 type TikTokTrending = Params[*tiktok.TrendingArguments]
 
-type TikTok = Params[base.JobArgument]
+type TikTokGenericArgs struct {
+	Data map[string]any `json:",inline"`
+}
+
+type TikTok = Params[TikTokGenericArgs]
+
+func (t TikTokGenericArgs) GetCapability() types.Capability {
+	return types.CapEmpty
+}
+
+func (t TikTokGenericArgs) SetDefaultValues() {
+}
+
+func (t TikTokGenericArgs) Validate() error {
+	return nil
+}
